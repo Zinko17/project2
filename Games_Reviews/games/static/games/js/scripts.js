8037687fd95b0aff29aef89dfbedbd51be5e9d16
Zinko17 <vovazin2@gmail.com>
2024-04-22 18:14:57 +0600

    some changes

--- Games_Reviews/games/static/games/js/scripts.js
+++ Games_Reviews/games/static/games/js/scripts.js
@@ -34,4 +34,23 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location.href = `/game/${gameId}/`; // Перенаправляем на страницу с подробным описанием игры
         });
     });
+
+        // Находим все элементы с классом game-card-image
+    const gameCardImages = document.querySelectorAll('.game-card-image');
+
+    // Перебираем найденные элементы
+    gameCardImages.forEach(image => {
+        // Добавляем обработчик события наведения мыши
+        image.addEventListener('mouseover', function () {
+            // Увеличиваем размер изображения
+            image.style.transform = 'scale(1.1)';
+            image.style.transition = 'transform 0.3s ease';
+        });
+
+        // Добавляем обработчик события ухода мыши
+        image.addEventListener('mouseout', function () {
+            // Возвращаем изначальный размер изображения
+            image.style.transform = 'scale(1)';
+        });
+    });
 });
